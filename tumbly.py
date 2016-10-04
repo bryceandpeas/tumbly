15219d2f4e1a06b1423913cfe21c14d302ea08b7
Bryce Fury <brycefury@ymail.com>
2016-10-04 07:00:36 +0100

    Revert "Add argparse to all"
    This reverts commit feae02e5d406b231b4da3f331f210156ef6a73e1.

--- tumbly.py
+++ tumbly.py
@@ -1,4 +1,3 @@
-import argparse
 from itertools import product
 import os
 import sqlite3
@@ -46,57 +45,27 @@ if __name__ == '__main__':
     start_offset = ''
     url_to_scrape = ''
 
-    # Init argparse
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-u',
-                        '--username',
-                        type=str,
-                        help='The username of the tumblr user whos '
-                             'tumblr you wish to scrape.',
-                        required=True)
-
-    parser.add_argument('-n',
-                        '--number',
-                        type=int,
-                        help='The number of images to scrape.',
-                        required=True)
-
-    parser.add_argument('-o',
-                        '--start',
-                        type=int,
-                        help='Post number to start from (offset).',
-                        required=False)
-
-    args = parser.parse_args()
-
-    # Set variables
-
-    username = args.username
-    number = args.number
-    offset = args.start
-
     # Get user input
     # Create URL
-    url_to_scrape = 'http://{0}.tumblr.com'.format(username)
+    url_to_scrape = 'http://{0}.tumblr.com'.format(sys.argv[1])
     print ('Will scrape: {0}'.format(url_to_scrape))
     # Create database name
-    database_name = '{0}.db'.format(username)
+    database_name = '{0}.db'.format(sys.argv[1])
     print ('Will save to: {0} database (SQLite3)'.format(database_name))
     # Check if directory exists, create if not
     script_directory = os.path.dirname(os.path.abspath(__file__))
     downloaded_image_directory = os.path.join(script_directory,
                                               '{0}_saved_images'
-                                              .format(username))
+                                              .format(sys.argv[1]))
 
     print('Will download images to: {0}'.format(downloaded_image_directory))
     if not os.path.exists(downloaded_image_directory):
         os.makedirs(downloaded_image_directory)
     # Get how many images the user wants to scrape
-    number_to_scrape = number
+    number_to_scrape = int(sys.argv[2])
     # If there is a third argument, set the offset
     if len(sys.argv) >= 4:
-        start_offset = offset
+        start_offset = int(sys.argv[3])
     else:
         start_offset = 0
 
