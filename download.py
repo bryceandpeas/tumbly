14a1bd51f2c55d514496908a589cf08e4e1021d4
Bryce Fury <brycefury@ymail.com>
2016-10-04 05:43:43 +0100

    Fix download to conform with PEP8

--- download.py
+++ download.py
@@ -5,25 +5,29 @@ import urllib.request
 
 ''' Downloading Functions '''
 
-def download_images(database_name, downloaded_image_directory, number_to_scrape):
-	# Connect to database
-	con = sqlite3.connect(database_name)
 
-	with con:    
-		# Get image urls from photos table
-		cur = con.cursor()    
-		cur.execute("SELECT url FROM photos")
+def download_images(database_name,
+                    downloaded_image_directory,
+                    number_to_scrape):
 
-		# Init J
-		j = 0
-		while j < number_to_scrape:
-			for url in cur:    
-				for i in url:
-					print('Image url: ' + i)
-					os.chdir(downloaded_image_directory)
-					image_name = (sys.argv[1] + '_' + str(j) + '.jpg')
-					urllib.request.urlretrieve(i, image_name)
-					print ('Downloaded: {0}'.format(image_name))
-					j += 1
+    # Connect to database
+    con = sqlite3.connect(database_name)
 
-		print('Finished scraping and downloading')
+    with con:
+        # Get image urls from photos table
+        cur = con.cursor()
+        cur.execute("SELECT url FROM photos")
+
+        # Init J
+        j = 0
+        while j < number_to_scrape:
+            for url in cur:
+                for i in url:
+                    print('Image url: ' + i)
+                    os.chdir(downloaded_image_directory)
+                    image_name = (sys.argv[1] + '_' + str(j) + '.jpg')
+                    urllib.request.urlretrieve(i, image_name)
+                    print ('Downloaded: {0}'.format(image_name))
+                    j += 1
+
+        print('Finished scraping and downloading')
