f507a0a545aa55ddeafa9b2a1f5eafad4c1fde7f
Bryce Fury <brycefury@ymail.com>
2016-10-04 06:58:18 +0100

    Remove unnecessary files

--- download.py
+++ download.py
@@ -1,7 +1,38 @@
+import argparse
 import sqlite3
 import sys
 import os
 import urllib.request
+
+# Init argparse
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-u',
+                    '--username',
+                    type=str,
+                    help='The username of the tumblr user whos '
+                         'tumblr you wish to scrape.',
+                    required=True)
+
+parser.add_argument('-n',
+                    '--number',
+                    type=int,
+                    help='The number of images to scrape.',
+                    required=True)
+
+parser.add_argument('-o',
+                    '--start',
+                    type=int,
+                    help='Post number to start from (offset).',
+                    required=False)
+
+args = parser.parse_args()
+
+# Set variables
+
+username = args.username
+number = args.number
+offset = args.start
 
 ''' Downloading Functions '''
 
@@ -25,7 +56,7 @@ def download_images(database_name,
                 for i in url:
                     print('Image url: ' + i)
                     os.chdir(downloaded_image_directory)
-                    image_name = (sys.argv[1] + '_' + str(j) + '.jpg')
+                    image_name = (username + '_' + str(j) + '.jpg')
                     urllib.request.urlretrieve(i, image_name)
                     print ('Downloaded: {0}'.format(image_name))
                     j += 1
