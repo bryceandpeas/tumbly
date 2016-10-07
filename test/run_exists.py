417a9af9774a3033b6a598a73dd94519cf13fee0
Bryce Fury <brycefury@ymail.com>
2016-10-07 05:18:18 +0100

    Add tests for file existence

---
+++ test/run_exists.py
@@ -0,0 +1,15 @@
+import os
+import unittest
+import tempfile
+import shutil
+
+
+class TumblyCLExists(unittest.TestCase):
+
+    def test_file(self):
+        file = os.path.isfile('../run.py')
+        self.assertTrue(file)
+
+
+if __name__ == '__main__':
+    unittest.main()
